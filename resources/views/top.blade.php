e46dc0fa78c31f6f8d5c12b095a88aba34334e71
KogaYuta <koga-yuta-np@ynu.jp>
2020-08-16 16:11:30 +0900

    view部分を作成

---
+++ resources/views/top.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+    <body>
+        <h1>S3基本操作</h1>
+        <form method="post">
+            <label for="s3">画像ファイル</label>
+            <input type="file" name="s3">
+            <input type="submit" name="upload" value="upload">
+        </form>
+    </body>
+</html>
